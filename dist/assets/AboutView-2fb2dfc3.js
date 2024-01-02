367ada1e8c87dba52c70c72d65bf3df9cb5fa6c9
Ahmed Atwa <aatwa86@gmail.com>
2024-01-02 19:03:18 +0000

    update

--- dist/assets/AboutView-2fb2dfc3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./main-cf5a2b6c.js";const c={},n={class:"container mx-auto"};function s(r,a){return o(),t("div",n,"About")}const i=e(c,[["render",s]]);export{i as default};
